4caa5df63e526278ee89baa3bf73bc03257252b8
LeonardoMachunac <lmgmendozagil@gmail.com>
2024-09-09 16:22:55 -0500

    se agrego en el campo index

--- admin/seccion/banners/index.php
+++ admin/seccion/banners/index.php
@@ -1,5 +1,18 @@
 <?php include ("../../templates/header.php");?>
-Listar Registros
+
+    <div class="card">
+        <div class="card-header">
+            <input name=""id=""class="btn btn-primary"type="button"value="Agregar Registros"/>
+            
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+        <div class="card-footer text-muted">Footer</div>
+    </div>
+    
+
 
 <?php include ("../../templates/footer.php"); ?>
 
