12d3496cb128a43677d91fc0869f3c32f8f464c9
LeonardoMachunac <lmgmendozagil@gmail.com>
2024-09-12 22:11:13 -0500

    implementacion de la logica de los colaboradores

--- admin/seccion/colaboradores/crear.php
+++ admin/seccion/colaboradores/crear.php
@@ -1,23 +1,65 @@
 <?php
+if($_POST){
+    print_r($_POST);
+}
+
+
 include ("../../templates/header.php");
 ?>
 <br/>
 <div class="card">
     <div class="card-header">Colaboradores</div>
     <div class="card-body">
+        <form action="" method="post" enctype="multipart/form-data"> <!--para adjuntar la foto-->
+            <div class="mb-3">
+                <label for="foto" class="form-label">Foto:</label>
+                <input type="file" class="form-control"name="foto"id="foto"placeholder=""aria-describedby="fileHelpId"/>
+            
+            </div>
+            
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo:</label>
+                <input type="text" class="form-control"name="titulo" id="titulo" aria-describedby="helpId" placeholder=""/>
+            
+            </div>
 
+
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripcion:</label>
+                <input type="text"class="form-control"name="descripcion"id="descripcion"aria-describedby="helpId"placeholder=""/>
+                
+            </div>
+            
+            <div class="mb-3">
+                <label for="linkfacebook" class="form-label">Facebook:</label>
+                <input type="text"class="form-control"name="linkfacebook"id="linkfacebook"aria-describedby="helpId" placeholder=""/>
+                
+            </div>
+            
+            <div class="mb-3">
+                <label for="linkinstagram" class="form-label">Instagram:</label>
+                <input type="text"class="form-control"name="linkinstagram"id="linkinstagram"aria-describedby="helpId" placeholder=""/>
+                
+            </div>
+
+            <div class="mb-3">
+                <label for="linklinkedin" class="form-label">Lnkedin:</label>
+                <input type="text"class="form-control"name="linklinkedin"id="linklinkedin"aria-describedby="helpId" placeholder=""/>
+                
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar Colaboradores </button>
+            <a name=""id=""class="btn btn-primary"href="index.php"role="button">Cancelar</a>
+            
+            
+            
+
+
+            
+        </form>
 
     </div>
-    <div class="card-footer text-muted">
-
-        <div class="mb-3">
-            <label for="" class="form-label">Choose file</label>
-            <input type="file" class="form-control"name=""id=""placeholder=""aria-describedby="fileHelpId"/>
-            <div id="fileHelpId" class="form-text">Help text</div>
-        </div>
-        
-
-    </div>
+    <div class="card-footer text-muted"></div>
 </div>
 
 
