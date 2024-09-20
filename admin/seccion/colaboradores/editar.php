bdc911a5407f937a062b58e345ccb67297c2bffa
LeonardoMachunac <lmgmendozagil@gmail.com>
2024-09-20 00:48:11 -0500

    en modificar se reemplaza la imagen

--- admin/seccion/colaboradores/editar.php
+++ admin/seccion/colaboradores/editar.php
@@ -1,13 +1,68 @@
 <?php 
 include ("../../bd.php");
     if($_POST){
+
+        $txtID=(isset($_POST["txtID"]))?$_POST["txtID"]:"";
         $titulo=(isset($_POST['titulo']))?$_POST['titulo']:""; 
         $descripcion=(isset($_POST['descripcion']))?$_POST['descripcion']:""; 
         $linkfacebook=(isset($_POST['linkfacebook']))?$_POST['linkfacebook']:""; 
         $linkinstagram=(isset($_POST['linkinstagram']))?$_POST['linkinstagram']:""; 
         $linklinkedin=(isset($_POST['linklinkedin']))?$_POST['linklinkedin']:"";
         
+        $sentencia=$conexion->prepare("UPDATE `tbl_colaboradores` 
+                SET 
+                titulo=:titulo, 
+                descripcion=:descripcion,
+                linkfacebook=:linkfacebook,
+                linkinstagram=:linkinstagram,
+                linklinkedin=:linklinkedin
+                WHERE ID=:id");
+
+        $sentencia->bindParam(":titulo",$titulo);
+        $sentencia->bindParam(":descripcion",$descripcion);
+        $sentencia->bindParam(":linkfacebook",$linkfacebookn);
+        $sentencia->bindParam(":linkinstagram",$linkinstagram);
+        $sentencia->bindParam(":linklinkedin",$linklinkedin);
+        $sentencia->bindParam(":id",$txtID);
+
+        $sentencia->execute();
+
+
+             //proceso de actualizacion de foto
+                $foto=(isset($_FILES['foto']["name"]))?$_FILES['foto']["name"]:"";
+                $tmp_foto=$_FILES["foto"]["tmp_name"];
+                
+                if($foto!=""){
+
+                    $fecha_foto= new DateTime();
+                    $nombre_foto=$fecha_foto->getTimestamp()."_".$foto;
+                    move_uploaded_file($tmp_foto,"../../../img/colaboradores/".$nombre_foto);
+        
+        
+                    $sentencia=$conexion->prepare("SELECT * FROM `tbl_colaboradores` WHERE ID=:id"); //seleccionamos estos colaboradores
+                    $sentencia->bindParam(":id",$txtID);
+                    $sentencia->execute();
+        
+                    $registro_foto=$sentencia->fetch(PDO::FETCH_LAZY);  //aislamos ese resultado //2recupera el archivo
+        
+                    if(isset($registro_foto['foto'])){ //1 valida si hay algo si existe el archivo
+                        if(file_exists("../../../img/colaboradores/".$registro_foto['foto'])){ //3. si existe en la carpeta
+                            unlink("../../../img/colaboradores/".$registro_foto['foto']);//4 borramos
+                        }
+                    }
+
+                    $sentencia=$conexion->prepare("UPDATE `tbl_colaboradores` 
+                    SET 
+                    foto=:foto 
+                    WHERE ID=:id");
+    
+                    $sentencia->bindParam(":foto",$nombre_foto);
+                    $sentencia->bindParam(":id",$txtID);
+                    $sentencia->execute();
+        }
     }
+
+
 
 
 
@@ -36,6 +91,11 @@ include ("../../templates/header.php"); ?>
     <div class="card-header">Colaboradores</div>
     <div class="card-body">
         <form action="" method="post" enctype="multipart/form-data"> <!--para adjuntar la foto-->
+
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo:</label>
+                <input type="text"class="form-control" value="<?php echo $txtID;?>" name="txtID"id="txtID" aria-describedby="helpId"placeholder="Escribe el titulo del banner"/>
+            </div>
 
             <div class="mb-3">
                 <label for="foto" class="form-label">Foto:</label><br/>
